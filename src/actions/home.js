6e3678d2cbe0ef8cc430a901da9b5e9278d6d250
zlbcc <2728104544@qq.com>
2020-01-09 20:50:21 +0800

    瀑布流

--- src/actions/home.js
+++ src/actions/home.js
@@ -1,9 +1,10 @@
 import { requestGet } from '@/utils/request'
+import { requestPost } from '@/utils/request'
 import { createActions } from 'redux-actions'
 import Apis from '@/services/api'
 
 export const home = createActions({
     // CESHI: options => request(services.listWithPage),
-    SET_DATA: options => options,
+    SET_DATA: options => requestPost(Apis.listData, options),
   })
 
--- src/pages/facebook/index.jsx
+++ src/pages/facebook/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './style.less'
-import Top from './top'
+import Top from '../../components/Top'
+import Buttom from '../../components/Buttom'
 export default class extends React.PureComponent {
   render () {
     return (
       <div className="pages-box">
           <Top/>
+          <Buttom/>
       </div>
     )
   }
